d72be0f725798b648f38c9684dac35666b0a5379
callmejkode <callmejkode@gmail.com>
2023-03-06 19:25:36 +0530

    home page component edits

--- ecommerce/src/components/homepage/Home.js
+++ ecommerce/src/components/homepage/Home.js
@@ -1,8 +1,18 @@
 import React from "react";
+import FourPromises from "./FourPromises";
 import HeroSection from "./HeroSection";
+import Services from "./Services";
+import Trusted from "./Trusted";
 
 function Home() {
-  return <HeroSection />;
+  return (
+    <>
+      <HeroSection />
+      <Services />
+      <FourPromises />
+      <Trusted />
+    </>
+  );
 }
 
 export default Home;
