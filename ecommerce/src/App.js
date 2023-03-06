381115a0921f6dbbc9f34ad234d9cdc05d99637e
callmejkode <callmejkode@gmail.com>
2023-03-06 13:26:22 +0530

    Router implemeted with basic main pages

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Cart from "./components/Cart";
+import Contact from "./components/Contact";
+import ErrorPage from "./components/ErrorPage";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import SingleProduct from "./components/SingleProduct";
 function App() {
   return (
-    <div className="">
-      <h1>JKode</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/singleproduct/:id" element={<SingleProduct />} />
+        <Route path="/about" element={<Cart />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
