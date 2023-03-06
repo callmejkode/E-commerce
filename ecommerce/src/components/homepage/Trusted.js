17e01136bdab5a3cac258c85fb567d4d0f2d4cfb
callmejkode <callmejkode@gmail.com>
2023-03-06 19:35:28 +0530

    trusted section content updated

--- ecommerce/src/components/homepage/Trusted.js
+++ ecommerce/src/components/homepage/Trusted.js
@@ -1,7 +1,55 @@
 import React from "react";
 
 function Trusted() {
-  return <div>Trusted</div>;
+  return (
+    <>
+      <p className="text-center pt-4 my-4">Trusted by 1000+ Companies</p>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <img
+              src="./praxis-design-company.jpg"
+              alt="Company Image"
+              width="200"
+              height="100"
+            />
+          </div>
+          <div className="col">
+            <img
+              src="./praxis-design-company.jpg"
+              alt="Company Image"
+              width="200"
+              height="100"
+            />
+          </div>
+          <div className="col">
+            <img
+              src="./praxis-design-company.jpg"
+              alt="Company Image"
+              width="200"
+              height="100"
+            />
+          </div>
+          <div className="col">
+            <img
+              src="./praxis-design-company.jpg"
+              alt="Company Image"
+              width="200"
+              height="100"
+            />
+          </div>
+          <div className="col">
+            <img
+              src="./praxis-design-company.jpg"
+              alt="Company Image"
+              width="200"
+              height="100"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Trusted;
